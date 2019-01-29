e22907a6baf52e753148becd85aa5022e89b1432
marianamatias <mmatias323@gmail.com>
2019-01-29 20:38:07 +0100

    attempt at installing firebase and cocoa pods

--- firstapp/AppDelegate.swift
+++ firstapp/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let frame = UIScreen.main.bounds
         window = UIWindow(frame: frame)
         
+        
         window!.rootViewController = mainVC
         window!.makeKeyAndVisible()
         // Override point for customization after application launch.
